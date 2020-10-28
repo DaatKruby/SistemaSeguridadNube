6062fbfbabfb900106416921097fc5551df81484
DaatKruby <marcos.garcia@potros.itson.edu.mx>
2020-10-28 11:54:12 -0700

    bugs fix

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
-const db = mongoose.connection;
+// const mongoose = require('mongoose');
+// const db = mongoose.connection;
 
-const nombre_bd= "SistemaSeguridad"
-mongoose.connect(`mongodb://localhost/SistemaSeguridad${nombre_bd}`, {useNewUrlParser: true});
+// const nombre_bd= "SistemaSeguridad"
+// mongoose.connect(`mongodb://localhost/SistemaSeguridad${nombre_bd}`, {useNewUrlParser: true});
 
-db.once("open", () => {
-    console.log(`Conectado correctamente a base de datos ${nombre_bd}`);
-}).on('error', (error) => {
-    console.error(`Error: ${error}`);
-});
+// db.once("open", () => {
+//     console.log(`Conectado correctamente a base de datos ${nombre_bd}`);
+// }).on('error', (error) => {
+//     console.error(`Error: ${error}`);
+// });
