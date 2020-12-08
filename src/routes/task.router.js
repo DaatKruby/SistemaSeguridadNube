279644f2e5f68d3d18558f1f652b9fc4f53c0111
DaatKruby <marcos.garcia@potros.itson.edu.mx>
2020-12-07 17:32:41 -0700

    finales rr

--- src/routes/task.router.js
+++ src/routes/task.router.js
@@ -2,7 +2,6 @@ const express = require('express');const router = express.Router();
 
 const Sensor = require('../models/infosensor');
 
-// const {isActividad} = require('../helper.js');
 
 router.get('/', async(req, res) => {
     const sensorInfo = await Sensor.find(); 
